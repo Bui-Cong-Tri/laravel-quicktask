198d6ea68ac10d281440f75dd1a4bb75a6b9130c
Luxion Rob <aidaynhi@gmail.com>
2023-07-18 15:55:45 +0700

    Chapter 7: Middleware

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+
+        if ($user) {
+            if ($user->is_admin) {
+                return $next($request);
+            }
+        }
+
+        return redirect('articles')->with('fail', 'You don\'t have permission to access this resources!');
+    }
+}
