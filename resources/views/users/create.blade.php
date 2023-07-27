ef92ce0dcf5c541bbb6756717d998a158ea963bb
Luxion Rob <aidaynhi@gmail.com>
2023-07-27 15:52:56 +0700

    Sort tailwind classes and format blade

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,44 +1,44 @@
 <x-app-layout>
 
-    <div class="container mx-auto px-4 py-8">
-        <h1 class="text-4xl font-bold mb-4">{{ __('message.user.create.title') }}</h1>
-        <form action="{{ route('users.store') }}" method="POST">
-            @csrf
+  <div class="container mx-auto px-4 py-8">
+    <h1 class="mb-4 text-4xl font-bold">{{ __('message.user.create.title') }}
+    </h1>
+    <form action="{{ route('users.store') }}" method="POST">
+      @csrf
 
-            <div class="mb-4">
-                <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-                    for="username">{{ __('message.user.username') }}</label>
-                <input
-                    class="appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline"
-                    id="username" name="username" type="text">
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-                    for="fullname">{{ __('message.user.fullName') }}:</label>
-                <input
-                    class="appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline"
-                    id="fullname" name="fullname" type="text">
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-                    for="email">Email:</label>
-                <input
-                    class="appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline"
-                    id="email" name="email" type="email">
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-                    for="password">{{ __('message.user.password') }}:</label>
-                <input
-                    class="appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline"
-                    id="password" name="password" type="password">
-            </div>
+      <div class="mb-4">
+        <label class="mb-2 block text-sm font-bold text-gray-700 dark:text-gray-300"
+          for="username">{{ __('message.user.username') }}</label>
+        <input
+          class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 focus:outline-none dark:text-gray-300"
+          id="username" name="username" type="text">
+      </div>
+      <div class="mb-4">
+        <label class="mb-2 block text-sm font-bold text-gray-700 dark:text-gray-300"
+          for="fullname">{{ __('message.user.fullName') }}:</label>
+        <input
+          class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 focus:outline-none dark:text-gray-300"
+          id="fullname" name="fullname" type="text">
+      </div>
+      <div class="mb-4">
+        <label class="mb-2 block text-sm font-bold text-gray-700 dark:text-gray-300"
+          for="email">Email:</label>
+        <input
+          class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 focus:outline-none dark:text-gray-300"
+          id="email" name="email" type="email">
+      </div>
+      <div class="mb-4">
+        <label class="mb-2 block text-sm font-bold text-gray-700 dark:text-gray-300"
+          for="password">{{ __('message.user.password') }}:</label>
+        <input
+          class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 focus:outline-none dark:text-gray-300"
+          id="password" name="password" type="password">
+      </div>
 
-            <!-- Add more input fields here -->
-
-            <button type="submit"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                {{ __('Create') }} </button>
-        </form>
-    </div>
+      <!-- Add more input fields here -->
+      <button type="submit"
+        class="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none">
+        {{ __('Create') }} </button>
+    </form>
+  </div>
 </x-app-layout>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,57 +1,60 @@
 <x-app-layout>
 
-    <div class="py-8">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white dark:bg-gray-900 border-b border-gray-200 dark:text-gray-400">
-                    <h2 class="text-2xl font-semibold mb-4">{{ __('message.user.show.title') }}</h2>
-                    <p class="dark:text-gray-600 group relative">
-                        <strong>{{ __('message.user.username') }}:</strong>
-                        {{ $user->username }}
-                        <span
-                            class="hidden group-hover:block -bottom-12 absolute p-2 bg-slate-400 rounded before:bg-slate-400 before:h-4 before:w-4 before:block before:rotate-45 before:absolute before:-top-1 before:right-2">
-                            {{ __('Create At') }} {{ formatDate($user->created_at) }}
-                        </span>
-                    </p>
-                    <p class="dark:text-gray-600"><strong>Email:</strong> {{ $user->email }}</p>
-                    <div class="flex justify-end mt-4 pb-4">
-                        <a href="{{ route('articles.createWithId', $user->id) }} "
-                            class="block mr-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                            {{ __('Create New Article') }}
-                        </a>
-                        <a href="{{ route('users.edit', ['user' => $user->id]) }}"
-                            class="block mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            {{ __('Edit') }}
-                        </a>
-                        <form method="POST"
-                            action="{{ route('users.destroy', $user->id) }}"class="block mr-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit"
-                                data-confirm="{{ __('Confirm Delete') }}">{{ __('Delete') }}</button>
-                        </form>
-                    </div>
-                    <h2 class="text-2xl
-                                font-semibold mt-8 mb-4">
-                        {{ __('message.user.show.article.title') }}</h2>
-                    @if ($user->articles->count() > 0)
-                        <ul class="divide-y divide-gray-200">
-                            @foreach ($user->articles as $article)
-                                <li class="py-4">
-                                    <h3 class="text-xl font-semibold"><a
-                                            href="{{ route('articles.show', $article->id) }}">{{ $article->title }}</a>
-                                    </h3>
-                                    <p class="text-gray-600 dark:text-gray-400">{{ $article->description }}
-                                    </p>
-                                </li>
-                            @endforeach
-                        </ul>
-                    @else
-                        <p class="text-gray-600 dark:text-gray-400">
-                            {{ __('message.user.show.article.empty') }}</p>
-                    @endif
-                </div>
-            </div>
+  <div class="py-8">
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+        <div class="border-b border-gray-200 bg-white p-6 dark:bg-gray-900 dark:text-gray-400">
+          <h2 class="mb-4 text-2xl font-semibold">
+            {{ __('message.user.show.title') }}</h2>
+          <p class="group relative dark:text-gray-600">
+            <strong>{{ __('message.user.username') }}:</strong>
+            {{ $user->username }}
+            <span
+              class="absolute -bottom-12 hidden rounded bg-slate-400 p-2 before:absolute before:-top-1 before:right-2 before:block before:h-4 before:w-4 before:rotate-45 before:bg-slate-400 group-hover:block">
+              {{ __('Create At') }}
+              {{ formatDate($user->created_at) }}
+            </span>
+          </p>
+          <p class="dark:text-gray-600"><strong>Email:</strong>
+            {{ $user->email }}</p>
+          <div class="mt-4 flex justify-end pb-4">
+            <a href="{{ route('articles.createWithId', $user->id) }} "
+              class="mr-2 block rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700">
+              {{ __('Create New Article') }}
+            </a>
+            <a href="{{ route('users.edit', ['user' => $user->id]) }}"
+              class="mr-2 block rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+              {{ __('Edit') }}
+            </a>
+            <form method="POST"
+              action="{{ route('users.destroy', $user->id) }}"class="block mr-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+              @csrf
+              @method('DELETE')
+              <button type="submit"
+                data-confirm="{{ __('Confirm Delete') }}">{{ __('Delete') }}</button>
+            </form>
+          </div>
+          <h2 class="mb-4 mt-8 text-2xl font-semibold">
+            {{ __('message.user.show.article.title') }}</h2>
+          @if ($user->articles->count() > 0)
+            <ul class="divide-y divide-gray-200">
+              @foreach ($user->articles as $article)
+                <li class="py-4">
+                  <h3 class="text-xl font-semibold"><a
+                      href="{{ route('articles.show', $article->id) }}">{{ $article->title }}</a>
+                  </h3>
+                  <p class="text-gray-600 dark:text-gray-400">
+                    {{ $article->description }}
+                  </p>
+                </li>
+              @endforeach
+            </ul>
+          @else
+            <p class="text-gray-600 dark:text-gray-400">
+              {{ __('message.user.show.article.empty') }}</p>
+          @endif
         </div>
+      </div>
     </div>
+  </div>
 </x-app-layout>
